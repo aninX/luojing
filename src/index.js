cf58b9bbf50e7253cb9f74514b7ebd77908d7e82
anin <xyyg12@163.com>
2023-12-22 19:34:54 +0800

    'line'

--- src/index.js
+++ src/index.js
@@ -2,14 +2,36 @@
 const defaultOptions = {
 
 }
-function luojing(container, option, data) {
+function setStyle (tag,style) {
+    return Object.assign(tag.style, style)
 
+}
+function luojing(container, option, data) {
+    let width = 50
     data.forEach((layer,index) => {
+        width += layer.width;
         const layerNode = document.createElement('div');
         layerNode.className = 'layer';
-        layerNode.style.width = `${layer.width}px`;
-        layerNode.style.height = `${layer.width}px`;
-        layerNode.style.zIndex = 99 - index;
+        const lstyle = {
+            width: `${width}px`,
+            height: `${width}px`,
+            rotate:  `${layer.rotate ?? 0}deg`,
+            zIndex: 99 -index,
+        }
+        setStyle(layerNode,lstyle);
+        for(let i = 0; i < layer.positions.length / 2;i++) {
+            const shan = document.createElement('div');
+            shan.className ='shan';
+            setStyle(shan, {
+                width:  `${width}px`,\
+                
+                height:  `${width}px`,
+                background:i === 0 ? layer?.bgColor?.[0] || 'none': 'none',
+                transform: `skew(${360 /layer.positions.length - 90}deg,0deg)`,
+                rotate:  `${720 /layer.positions.length * i + (180 / layer.positions.length )}deg`
+            });
+            layerNode.appendChild(shan);
+        }
         if (layer.positions.length % 2 == 1) {
             layer.positions.forEach(position => {
                 const div = document.createElement('div');
@@ -27,31 +49,59 @@ function luojing(container, option, data) {
                 right.innerHTML = layer.positions[layer.positions.length / 2 + i];
                 left.className = 'left'
                 right.className = 'right';
-                div.style.transform = `rotateZ(${360 / layer.positions.length * i}deg)`;
-                left.style.transform = `rotateZ(-90deg)`;
-                right.style.transform = `rotateZ(90deg)`;
+                setStyle(div,{transform: `rotateZ(${360 / layer.positions.length * i}deg)`,})
+                setStyle(left,{transform: `rotateZ(-90deg)`})
+                setStyle(right,{transform:`rotateZ(90deg)`})
+                // div.style.transform = `rotateZ(${360 / layer.positions.length * i}deg)`;
+                // left.style.transform = `rotateZ(-90deg)`;
+                // right.style.transform = `rotateZ(90deg)`;
                 div.appendChild(left);
                 div.appendChild(right);
                 layerNode.appendChild(div)
             }
         }
+       
 
         container.appendChild(layerNode)
 
-        const yuan =  document.createElement('div');
-        yuan.style.width = `${layer.width}px`
-        yuan.style.height =`${layer.width}px`
-        yuan.style.zIndex = 99 - index;
-        yuan.className = 'yuan'
-        for(let i = 0; i < layer.positions.length / 2;i++) {
-            const line =  document.createElement('div');
-            line.style.transform = `rotateZ(${360 / layer.positions.length * i + (180 / layer.positions.length )}deg)`;
-           
-            yuan.appendChild(line);
-        }
-        container.appendChild(yuan)
+        // const yuan =  document.createElement('div');
+        // setStyle(yuan,lstyle);
+        // yuan.className = 'yuan'
+        // for(let i = 0; i < layer.positions.length / 2;i++) {
+        //     const line =  document.createElement('div');
+        //     line.style.transform = `rotateZ(${360 / layer.positions.length * i + (180 / layer.positions.length )}deg)`;
+        //     yuan.appendChild(line);
+        // }
+        // container.appendChild(yuan)
+      
 
     });
     
+}
+
+export function draw (container,option,data) {
+    let width = 50;
+    data.forEach((layer,index) =>{
+        width += layer.width;
+        const quan = document.createElement('div');
+        quan.className='quan';
+        setStyle(quan,{
+            width: `${width}px`,
+            height: `${width}px`,
+        })
+        layer.positions.forEach((pos, i) => {
+            const shan = document.createElement('div');
+            shan.className ='shan';
+            setStyle(shan, {
+                width:  `${width}px`,
+                height:  `${width}px`,
+                transform: `skew(${90 - 360 /layer.positions.length}deg,0deg)`,
+                rotate:  `${360 /layer.positions.length * i}deg`
+            });
+            shan.innerHTML = pos;
+            quan.appendChild(shan)
+        })
+        container.appendChild(quan)
+    })
 }
 export default luojing;
